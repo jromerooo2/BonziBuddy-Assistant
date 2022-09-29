0140491ed2e35401ffd86eeb82a85060b2ecff0c
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-09-28 22:23:51 -0500

    commenting who the heck is cmd

--- assistant.py
+++ assistant.py
@@ -33,28 +33,29 @@ def take_command():
 
 def run_alexa():
     command = take_command()
-    if command == "":
-        talk("Sorry, I didn't get that")
-    if 'play' in command:
-        song = command.replace('play', '')
-        talk('playing ' + song)
-        pywhatkit.playonyt(song)
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        talk('Current time is ' + time)
-    elif 'who the heck is' in command:
-        person = command.replace('who the heck is', '')
-        info = wikipedia.summary(person, 1)
-        print(info)
-        talk(info)
-    elif 'date' in command:
-        talk('sorry, I have a headache')
-    elif 'are you single' in command:
-        talk("What do u want lol")
-        talk('I am in a relationship with wifi')
-    elif 'joke' in command:
-        talk(pyjokes.get_joke())
-
+    try:
+        if command == "":
+            talk("Sorry, I didn't get that")
+        if 'play' in command:
+            song = command.replace('play', '')
+            talk('playing ' + song)
+            pywhatkit.playonyt(song)
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            talk('Current time is ' + time)
+        # elif 'who the heck is' in command:
+        #     person = command.replace('who the heck is', '')
+        #     info = wikipedia.summary(person, 1)
+        #     talk(info)
+        elif 'date' in command:
+            talk('sorry, I have a headache')
+        elif 'are you single' in command:
+            talk("What do u want lol")
+            talk('I am in a relationship with wifi')
+        elif 'joke' in command:
+            talk(pyjokes.get_joke())
+    except NameError:
+        talk("Can you repeat that please?")
 
 while True:      
     run_alexa()
