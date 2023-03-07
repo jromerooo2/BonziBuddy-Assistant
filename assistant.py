857562f868b5f00bbb06dbb06361fedc2a74a119
Jonathan Stephens <83253662+JDStephens31@users.noreply.github.com>
2023-03-06 19:01:07 -0500

    Init Commit

--- assistant.py
+++ assistant.py
@@ -1,14 +1,20 @@
-import speech_recognition as sr
+import datetime
+import json
+import webbrowser
+from time import sleep
+import pyautogui
+import pyjokes
 import pyttsx3
 import pywhatkit
-import datetime
-import wikipedia
-import pyjokes
+import speech_recognition as sr
+import chatGPT
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+wake_word = "test"
 
 
 def talk(text):
@@ -16,17 +22,18 @@ def talk(text):
     engine.runAndWait()
 
 
-def get_feedback(joke):
+def get_response():
     try:
         with sr.Microphone() as source:
-            print("Listening to your feedback")
-            voice  = listener.listen(source)
+            print("Listening to your Response")
+            voice = listener.listen(source)
             feed = listener.recognize_google(voice)
-            feed =  feed.lower()
-            talk("I really appreciate your feedback")
-            talk(joke)
+            feed = feed.lower()
     except NameError:
         print("An exception occurred")
+    return feed
+
+
 def take_command():
     try:
         with sr.Microphone() as source:
@@ -34,8 +41,8 @@ def take_command():
             voice = listener.listen(source)
             cmd = listener.recognize_google(voice)
             cmd = cmd.lower()
-            if 'bonzibuddy' in cmd:
-                cmd = cmd.replace('bonzibuddy', '')
+            if wake_word in cmd:
+                cmd = cmd.replace(wake_word, '')
                 print(cmd)
     except NameError:
         print("An exception occurred")
@@ -45,37 +52,66 @@ def take_command():
 def run_alexa():
     command = take_command()
     try:
-        if command == "bonzibuddy":
+        # Said Only Wake word
+        if command == wake_word:
             talk("Sorry, I didn't get that")
+
+        # Will open YouTube with a random video from the channel
         if 'play' in command:
             song = command.replace('play', '')
             talk('playing ' + song)
             pywhatkit.playonyt(song)
-        elif 'open' in command:
-            cmd = command.replace('open', '')
-            pywhatkit.search(cmd)
+
+        # Will ask ChatGPT for information on thing
+        elif 'what is' in command:
+            talk(chatGPT.new_chat(command))
+
+        # Will return time
         elif 'time' in command:
             time = datetime.datetime.now().strftime('%I:%M %p')
             talk('Current time is ' + time)
-        elif 'loco' in command:
-            talk('Fuck you too')
-        #TODO: Check what is happening here...
-        # elif 'who the heck is' in command:
-        #     person = command.replace('who the heck is', '')
-        #     info = wikipedia.summary(person, 1)
-        #     talk(info)
-        elif 'date' in command:
-            talk('sorry, I have a headache')
-        elif 'are you single' in command:
-            talk("What do u want lol")
-            talk('I am in a relationship with wifi')
+
+        # Will message someone (Person will need to be in contact.json file to message)
+        elif 'message' in command:
+            cmd = command.replace('message', '')
+            cmd = cmd.strip()
+            with open('contact.json') as f:
+                data = json.load(f)
+            try:
+                webbrowser.open(data['Contacts'][cmd]['link'])
+            except NameError:
+                talk("An Error Occurred.")
+                return
+            sleep(4)
+            pyautogui.click(590, 1000)
+            talk("What would you like to say?")
+            try:
+                response = get_response()
+                pyautogui.write(response)
+            except sr.UnknownValueError:
+                talk("An error occurred. ")
+                return
+
+            sleep(1)
+            pyautogui.press('enter')
+
+        # Will tell a joke
         elif 'joke' in command:
             joke = pyjokes.get_joke()
             talk(joke)
             talk("Did you enjoy the joke?")
-            get_feedback(joke)
+
+        # Will write a paper based on your parameters and store/open the file when done
+        elif 'write a paper' in command:
+            chatGPT.write_paper(command)
+
+        # Did not understand
+        else:
+            talk("Sorry I did not understand that.")
+            return
     except NameError:
         talk("Can you repeat that please?")
 
-while True:      
+
+while True:
     run_alexa()
---
+++ chatGPT.py
@@ -0,0 +1,55 @@
+import random
+import openai
+import os
+
+# OpenAI Initialization
+openai.organization = "org-vxtFFnnBThf1eKODPmnmi6R3"
+openai.api_key = "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"
+
+
+def new_chat(prompt):
+    chatResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="\nHuman: " + prompt + "\nAI:",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    return chatResponse['choices'][0]['text']
+
+
+def write_paper(prompt):
+    chatResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="\nHuman: " + prompt + "\nAI:",
+        temperature=0.9,
+        max_tokens=5000,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"]
+    )
+    number = random.randrange(1, 100)
+    file_name = "paper" + str(number)
+    file_path = "./Papers/" + file_name + ".txt"
+    f = open("./Papers/" + file_name + ".txt", "w+")
+    f.write(chatResponse['choices'][0]['text'])
+    f.close()
+    osCommandString = "notepad.exe " + file_path + ""
+    os.system(osCommandString)
+
+
+def summarize(prompt):
+    chatResponse = openai.Completion.create(
+        model="text-davinci-003",
+        prompt="Summarize this text:\n\n " + prompt,
+        temperature=0.5,
+        max_tokens=64,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+    )
+    return chatResponse['choices'][0]['text']
